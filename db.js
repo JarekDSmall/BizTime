eca12a1f35aab9c3a5f9a071bf7c6ac17679396a
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-08-24 18:02:23 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,11 @@
 /** Database setup for BizTime. */
-const { Client } = require('pg');
-
-const DB_USER = 'your_username';
-const DB_PASSWORD = 'your_password';
-
-const DB_URI = process.env.NODE_ENV === 'test'
-  ? 'postgresql://localhost/biztime_test'
-  : `postgresql://${DB_USER}:${DB_PASSWORD}@localhost/biztime`;
+const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: DB_URI
+  connectionString: "postgresql:///biztime"
 });
 
 client.connect();
+
 
 module.exports = client;
