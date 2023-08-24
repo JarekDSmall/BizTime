73dc41891ba429ad6f8f3441067621dd9ddc4c91
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-08-24 16:55:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 /** BizTime express application. */
-
-
-const express = require("express");
-
+const express = require('express');
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require('./expressError');
+const companiesRoutes = require('./routes/companies');
+const invoicesRoutes = require('./routes/invoices');
 
 app.use(express.json());
 
+// Mount company and invoice routers
+app.use('/companies', companiesRoutes);
+app.use('/invoices', invoicesRoutes);
 
 /** 404 handler */
-
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
+app.use(function (req, res, next) {
+  const err = new ExpressError('Not Found', 404);
   return next(err);
 });
 
-/** general error handler */
-
+/** General error handler */
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-
   return res.json({
     error: err,
-    message: err.message
+    message: err.message,
   });
 });
-
 
 module.exports = app;
